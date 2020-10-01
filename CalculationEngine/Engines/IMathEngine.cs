be34976c093eb5a8d3b31d86445ff317e291e2e0
Don <donrolling@gmail.com>
2020-10-01 14:43:18 -0500

    Adding the math engine

---
+++ CalculationEngine/Engines/IMathEngine.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculationEngine
+{
+    public interface IMathEngine
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        decimal Divide(int a, int b);
+        decimal Add(decimal a, decimal b);
+        decimal Subtract(decimal a, decimal b);
+        decimal Multiply(decimal a, decimal b);
+        decimal Divide(decimal a, decimal b);
+    }
+}
---
+++ CalculationEngine/Engines/MathEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalculationEngine
+{
+    public class MathEngine : IMathEngine
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public decimal Divide(int a, int b)
+        {
+            return (decimal)a / (decimal)b;
+        }
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public decimal Multiply(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public decimal Subtract(decimal a, decimal b)
+        {
+            return a - b;
+        }
+    }
+}
